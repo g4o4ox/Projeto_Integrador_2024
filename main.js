4a43727ae36eb2df15d4326f83eb3e4a1e32a401
GROX <103297099+g4o4ox@users.noreply.github.com>
2024-08-12 14:00:04 -0300

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-
-alert("Cuidado tem um criminoso tentando roubar sua senha")
-document.write("Vamos pegar sua senha e guardar ela com muito carinho")
-let nome = prompt("Mande sua senha: ")
-alert("Sua senha é "+nome+" ela foi roubada")
